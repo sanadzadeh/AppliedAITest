07988123ea1d045916ab4bb410ef0f280669b0c5
sanadzadeh <138832352+sanadzadeh@users.noreply.github.com>
2023-07-11 15:37:05 +1000

    add eng to fr translate

--- translator.py
+++ translator.py
@@ -1,1 +1,6 @@
 from deep_translator import MyMemoryTranslator
+
+def english_to_french(EngText):
+    translator = MyMemoryTranslator(source='en', target='fr')
+    FrTranslation = translator.translate(EngText)
+    return FrTranslation

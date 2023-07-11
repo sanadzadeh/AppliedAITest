402e2922780295e02c02669ea64dd6650202fd78
sanadzadeh <138832352+sanadzadeh@users.noreply.github.com>
2023-07-11 15:44:00 +1000

    Update translator.py

--- translator.py
+++ translator.py
@@ -4,3 +4,8 @@ def english_to_french(EngText):
     translator = MyMemoryTranslator(source='en', target='fr')
     FrTranslation = translator.translate(EngText)
     return FrTranslation
+
+def french_to_english(FrText):
+    translator = MyMemoryTranslator(source='fr', target='en')
+    EnTranslation = translator.translate(FrText)
+    return EnTranslation
